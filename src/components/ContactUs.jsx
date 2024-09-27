b572e83d9003fd21ae8647f7e735e7e378d9ead2
Aically <icalandrian20@gmail.com>
2024-09-27 11:53:30 +0800

    first commit

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ContactUs() {
+  return (
+    <div className="content">
+      <h1>Contact Us</h1>
+      <p>Reach out to us anytime. We'd love to hear from you!</p>
+    </div>
+  );
+}
+
+export default ContactUs;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="content">
+      <h1>Home Page</h1>
+      <p>Welcome to the Home page of our website.!</p>
+    </div>
+  );
+}
+
+export default Home;
